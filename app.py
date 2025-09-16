438cdfdcfc209a8c1cc880179ee04141b9858f8b
Varun S <varunshanmugam643@gmail.com>
2025-09-16 15:25:27 +0530

    commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import joblib
 
-# Load saved models and encoders
 model = joblib.load('best_obesity_model.pkl')
 scaler = joblib.load('scaler.pkl')
 label_encoders = joblib.load('label_encoders.pkl')
@@ -17,26 +16,21 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Collect input data from form
         input_data = {}
         for col in ['Gender','Age','Height','Weight','family_history_with_overweight','FAVC','FCVC','NCP','CAEC','SMOKE','CH2O','SCC','FAF','TUE','CALC','MTRANS']:
             input_data[col] = [request.form[col]]
 
         df_input = pd.DataFrame(input_data)
 
-        # Encode categorical features
         for col, le in label_encoders.items():
             if col in df_input.columns:
                 df_input[col] = le.transform(df_input[col])
 
-        # Feature scaling
         X_scaled = scaler.transform(df_input)
 
-        # Make prediction
         prediction = model.predict(X_scaled)
         predicted_class = prediction[0]
 
-        # Decode predicted class
         for col, le in label_encoders.items():
             if col == 'NObeyesdad':
                 predicted_class = le.inverse_transform([predicted_class])[0]
